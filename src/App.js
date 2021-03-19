86efa128e5493da8777abe076df1ee11df4898b6
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-18 22:19:32 -0400

    add apply memoization

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import "./App.css";
 import "./custom.scss";
 import github from "./db.js";
-import React, { useEffect, useState } from "react";
+import githubQuery from "./Query.js";
+import React, { useEffect, useState, useCallback } from "react";
 
 function App() {
   let [userName, setUserName] = useState("");
-  useEffect(() => {
-    const githubQuery = {
-      query: `
-      {
-        viewer{
-          name
-        }
-      }
-      `,
-    };
+
+  let fetchData = useCallback(() => {
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
@@ -22,18 +15,24 @@ function App() {
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         setUserName(data.data.viewer.name);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <h1>Hey there {userName}</h1>
     </div>
   );
 }
